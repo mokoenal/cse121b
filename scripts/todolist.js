2f608880cc1f71effcc22bf08b2b8da620604447
mokoenal <lindiwemokoena374@gmail.com>
2023-10-24 21:54:57 +0200

    added to the folder

--- scripts/todolist.js
+++ scripts/todolist.js
@@ -1,47 +1,65 @@
-const taskList = document.getElementById("task-list");
-const newTaskInput = document.getElementById("new-task");
-const addTaskButton = document.getElementById("add-task");
+// Get references to the DOM elements
+const taskInput = document.getElementById('task');
+const addTaskButton = document.getElementById('add-task');
+const taskList = document.getElementById('tasks');
 
-// Function to fetch tasks from an external JSON source using the fetch API
-function fetchTasks() {
-    // Use the fetch API to get data from your JSON source
-    // Parse the data and render it on the web page
-}
+// Event listener to add a new task
+addTaskButton.addEventListener('click', function () {
+  const taskText = taskInput.value.trim();
 
-// Function to render tasks on the web page
-function renderTasks(tasks) {
-    // Loop through tasks and create HTML elements for each task
-}
+  if (taskText !== '') {
+    // Create a new task element
+    const taskItem = document.createElement('li');
+    taskItem.textContent = taskText;
 
-// Function to add a new task
-function addTask() {
-    // Get the task text from the input field
-    // Create a new task object
-    // Update the UI and send a PUT request to update the JSON source
-}
+    // Create a button to mark the task as complete
+    const completeButton = document.createElement('button');
+    completeButton.textContent = 'Complete';
+    completeButton.addEventListener('click', function () {
+      taskItem.classList.add('completed');
+      taskItem.style.textDecoration = 'line-through'; // Add line-through text decoration
+    });
 
-// Function to mark a task as completed
-function markTaskCompleted(taskId) {
-    // Update the UI and send a PUT request to update the JSON source
-}
+    // Create a button to delete the task
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.addEventListener('click', function () {
+      taskList.removeChild(taskItem);
+    });
 
-// Function to delete a task
-function deleteTask(taskId) {
-    // Update the UI and send a DELETE request to update the JSON source
-}
+    // Append the buttons to the task item
+    taskItem.appendChild(completeButton);
+    taskItem.appendChild(deleteButton);
 
-// Event listeners
-addTaskButton.addEventListener("click", addTask);
-taskList.addEventListener("click", (event) => {
-    if (event.target.classList.contains("completed")) {
-        const taskId = event.target.getAttribute("data-id");
-        markTaskCompleted(taskId);
-    }
-    if (event.target.classList.contains("delete")) {
-        const taskId = event.target.getAttribute("data-id");
-        deleteTask(taskId);
-    }
+    // Add the task to the task list
+    taskList.appendChild(taskItem);
+
+    // Clear the input field
+    taskInput.value = '';
+  }
 });
 
-// Fetch tasks when the page loads
-fetchTasks();
+// Fetch the JSON data from your file
+fetch('https://raw.githubusercontent.com/mokoenal/todolist.json/main/project')
+  .then((response) => response.json())
+  .then((data) => {
+    // Iterate through the data and create list items for each task
+    data.forEach((task) => {
+      const taskItem = document.createElement('li');
+      taskItem.textContent = task.task;
+
+      if (task.completed) {
+        taskItem.classList.add('completed');
+        taskItem.style.textDecoration = 'line-through'; // Add line-through text decoration
+      }
+
+      // Create buttons for marking tasks as complete and deleting them
+      // ... (your existing code for this)
+
+      // Append the task item to your task list
+      taskList.appendChild(taskItem);
+    });
+  })
+  .catch((error) => {
+    console.error('Error fetching JSON data:', error);
+  });
